6874fbe482ce09161574f7818212d35cfc4e4a4b
plashdof <plashdof@gmail.com>
2022-07-19 23:26:14 +0900

    중복POST 오류 해결. React.Strict 삭제

--- src/index.js
+++ src/index.js
@@ -9,10 +9,12 @@ import {BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    
+    <App />
+    
   </BrowserRouter>
+  
+  // React.Strict 로 감싸면, 개발자모드로서 rendering이 두번된다. 두번 Post하는 오류 해결. 하지만 근본적인 해결책은 아님
   
 );
 
