d68b7069c824f46651d086c400b9f48ac96243c1
wldn143 <wldn143@naver.com>
2022-07-24 18:51:09 +0900

    잔고확인 페이지 구현

--- src/pages/Balance.js
+++ src/pages/Balance.js
@@ -1,9 +1,36 @@
 import AfterLogin from "../component/layout/AfterLogin";
+import styled from "styled-components";
+import BalanceInfo from "../component/BalanceElement/BalanceInfo"
+import StocksInfo from "../component/BalanceElement/StocksInfo"
 
+const Title=styled.div`
+    width:25%;
+    display:flex;
+    justify-content:center;
+`
+const Contents=styled.div`
+    width:75%;
+    margin-left: 40px;
+`
+const Layout=styled.div`
+    display:flex;
+    flex-direction:row;
+    margin-top:70px
+`
 function BalancePage(){
     return <div>
         <AfterLogin>
-            balance Page 
+            <Layout>
+            <Title>
+                <div style={{fontSize:"30px", fontWeight:"bold"}}>
+                📒 잔고확인
+                </div>
+            </Title>
+            <Contents>
+            <BalanceInfo/>
+            <StocksInfo/>
+            </Contents>
+            </Layout>
         </AfterLogin> 
     </div>
 }
