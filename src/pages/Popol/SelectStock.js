42a76d94a21dda0487be058ac9bba043bcd3c221
plashdof <plashdof@gmail.com>
2022-07-27 22:45:56 +0900

    popol page 구현중

---
+++ src/pages/Popol/SelectStock.js
@@ -0,0 +1,12 @@
+import AfterLogin from "../../component/layout/AfterLogin";
+
+function SelectStockPage(){
+    return <div> 
+        <AfterLogin>
+            Select Page
+        </AfterLogin>
+
+    </div>
+}
+
+export default SelectStockPage;
