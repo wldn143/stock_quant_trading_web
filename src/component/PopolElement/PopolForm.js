582bdf7621c984391b806f8b77d561bb221d332e
plashdof <plashdof@gmail.com>
2022-07-29 03:42:41 +0900

    자동완성, 장바구니 기능 추가

--- src/component/PopolElement/PopolForm.js
+++ src/component/PopolElement/PopolForm.js
@@ -5,7 +5,7 @@ import ErrorModal from './Modals/ErrorModal';
 
 import styled from 'styled-components';
 
-const SelectButton=styled.button`
+const SelectButton=styled.button`               
 
     border: 1px solid #D8D8D8;
     border-radius: 10px;
@@ -14,7 +14,7 @@ const SelectButton=styled.button`
     transition: background-color 0.5s;
     margin-top: 50px;
 
-    background-color: ${props => {
+    background-color: ${props => {              // 가변디자인. 버튼눌르면 버튼색바뀜
         if(props.Clicked === 1){
             return '#b39b9a';
         }
@@ -39,42 +39,42 @@ function PopolForm(){
 
     const navigate = useNavigate();
 
-    let [sttData, setsttData] = useState(null);
-    let [Clicked, setClicked] = useState([0,0]);
+    let [sttData, setsttData] = useState(null);         // 전략종류 저장하는 변수
+    let [Clicked, setClicked] = useState([0,0]);        
 
-    let [OpenModal, setOpenModal] = useState(false);
+    let [OpenModal, setOpenModal] = useState(false);     // Modal 상태 저장 변수
     
 
-    function selectstt1(e){
+    function selectstt1(e){             // 전략1 버튼 클릭
         setsttData("stt1");
         setClicked([1,0]);
     }
 
-    function selectstt2(e){
+    function selectstt2(e){             // 전략2 버튼 클릭
         setsttData("stt2");
         setClicked([0,1]);
     }
 
-    function CloseModal(){
+    function CloseModal(){              // Modal창 닫기버튼 클릭
         setOpenModal(false);
     }
 
-    function submitHandler(e){
+    function submitHandler(e){          
         e.preventDefault();
 
-        if(sttData === null){
+        if(sttData === null){           //  선택 안하고 다음단계 버튼 클릭시 modal창 띄우고 종료
             setOpenModal(true);
             return;
         }
 
-        fetch('https://stock-a95d6-default-rtdb.firebaseio.com/.json',{
+        fetch('https://stock-a95d6-default-rtdb.firebaseio.com/.json',{        // 선택 잘 했을때, 서버로 어떤저략인지 데이터 POST. 
             method:'POST',
             body:JSON.stringify({sttData}),
             headers:{
                 'Content-type': 'application/json'
             }
-        }).then(navigate('/home/popol/select'))
-        .catch()
+        }).then(navigate('/home/popol/select'))             // 그후 장바구니 페이지로 이동
+        
     }
 
     
@@ -90,7 +90,7 @@ function PopolForm(){
 
                 <h1> 사용하실 전략을 선택하세요 </h1>
 
-                <SelectButton Clicked={Clicked[0]} onClick={selectstt1}>
+                <SelectButton Clicked={Clicked[0]} onClick={selectstt1}>        
                     <h2>전략1</h2>
                     <h3>월간 수익률</h3>
                     <h3>변동 </h3>
--- src/component/PopolElement/SelectStockForm.js
+++ src/component/PopolElement/SelectStockForm.js
@@ -4,40 +4,63 @@ import classes from "./SelectStockForm.module.css"
 import {useState} from 'react';
 import Modal from "./Modals/Modal";
 
+
+
 function SelectStockForm(){
 
-    const [modalOpen, setModalOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);             // 자본선택 modal 창 관리 변수
 
-    const openModal = () => {
+    const [searchResult, setsearchResult] = useState([]);         //  검색리스트 저장 변수
+    const [stockCart, setstockCart] = useState([]);             //   장바구니리스트 저장 변수
+
+
+
+    const openModal = () => {     
       setModalOpen(true);
     };
+
     const closeModal = () => {
       setModalOpen(false);
     };
 
+    function selectHandler(params){             // 검색리스트에서 주식 클릭시, 해당 주식명 params으로 받기
+        if(!stockCart.includes(params)){            //  장바구니리스트에 이미 있는 주식인지 체크
+            setstockCart([...stockCart, params]);       // 장바구니리스트 추가
+        }              
+    }
+
+    function deleteHandler(params){             //  장바구니 리스트에서 x 버튼 클릭시, 해당 주식 삭제
+        setstockCart(stockCart.filter(x=>x !== params));    
+    }
+
    
     
 
+    // map을 이용하여,  변경사항 반영된 검색리스트, 장바구니리스트  화면에 출력
+
     return(
         <>
-            <SearchBar/>
+            <SearchBar setsearchResult={setsearchResult}/>
             <section className={classes.box} >
                 <ul className={classes.resultList}>
-                    <li>삼성전자 : 50000 <span></span></li>
-                    <li>LG : 140000</li>
-                    <li>sdf</li>
-                    <li>sdf</li>
-                    <li>sdf</li>
+                    {searchResult.map((item) =>{
+                        return(
+                            <li onClick={()=>selectHandler(item)}>{item}</li>
+                        )
+                    }) }
+
                 </ul>
 
-                <h2> 장바구니 </h2>
+                
 
                 <ul className={classes.cartList}>
-                    <li>삼성전자 : 50000 <span></span></li>
-                    <li>LG : 140000</li>
-                    <li>sdf</li>
-                    <li>sdf</li>
-                    <li>sdf</li>
+                    <h2> 장바구니 </h2>
+
+                    {stockCart.map((item)=>{
+                        return(
+                            <li><span>{item}</span> <button onClick={()=>deleteHandler(item)}>x</button> </li>
+                        )
+                    })}
                 </ul>
 
                 <button className={classes.submitbtn} onClick={openModal}>선택 완료
