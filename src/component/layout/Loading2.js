34d830ed20d20b1863d6f0255432185ebcba0167
wldn143 <wldn143@naver.com>
2022-08-03 18:06:31 +0900

    검색창 활성화 수정

--- src/component/layout/Loading2.js
+++ src/component/layout/Loading2.js
@@ -2,9 +2,11 @@ import spinner from './spinner2.gif';
 
 function Loading2(){
     return(
-        <div style={{display:"flex",alignItems:"center"}}>
+        <div style={{display:"flex",alignItems:"center", justifyContent: "center", width:"100%", height:"400%"}}>
             <img style={{height:"25px",width:"25px"}} src={spinner} alt='로딩중' width='70px' />
         </div>
-    )
+    )//loading in search result container
+    // height 0px error
+    //focus off
 }
 export default Loading2;
