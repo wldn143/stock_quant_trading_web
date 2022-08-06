2db74e0b878bd12227f91b45f7e6d40cc48b3005
박진성 <plashdof@gmail.com>
2022-08-06 22:44:03 +0900

    TODO리스트 수정

--- src/component/PopolElement/SelectStockForm.js
+++ src/component/PopolElement/SelectStockForm.js
@@ -30,9 +30,7 @@ const EnjoybtnOff=styled.button`
 `
 
 
-// TODO 1 : 장바구니 주식 리스트, 서버로 전송하는 로직. (설정금액과 한꺼번에 전송할건지 아닌지)
-
-// TODO 2 : 주식리스트, 주식가격, 즐겨찾기 버튼   하나의 바로 보이게 디자인 수정
+// TODO 1 : 주식리스트 hover 처리
 
 
 function SelectStockForm(){
