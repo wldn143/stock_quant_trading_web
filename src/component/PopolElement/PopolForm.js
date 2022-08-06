481217c106a6094f5bea9fa864884d3a77b71dde
p.real_sound <plashdof@gmail.com>
2022-08-06 17:05:30 +0900

    popolpage request 방식 수정

--- src/component/PopolElement/PopolForm.js
+++ src/component/PopolElement/PopolForm.js
@@ -42,20 +42,25 @@ function PopolForm(){
     const navigate = useNavigate();
 
     let [sttData, setsttData] = useState(null);         // 전략종류 저장하는 변수
-    let [Clicked, setClicked] = useState([0,0]);        
+    let [Clicked, setClicked] = useState([0,0]);  
+    let [sttList, setsttList] = useState([]);      
 
     let [OpenModal, setOpenModal] = useState(false);     // Modal 상태 저장 변수
     
 
-    function selectstt1(e){             // 전략1 버튼 클릭
-        setsttData("stt1");
-        setClicked([1,0]);
+    function selectstt(idx, name){             // 전략1 버튼 클릭
+        setsttData(name);
+        let temp = [];
+        for(let i = 0; i < Clicked.length; i++){
+            if(i===idx){
+                temp.push(1);
+                continue;
+            }
+            temp.push(0);
+        }
+        setClicked(temp);
     }
 
-    function selectstt2(e){             // 전략2 버튼 클릭
-        setsttData("stt2");
-        setClicked([0,1]);
-    }
 
     function CloseModal(){              // Modal창 닫기버튼 클릭
         setOpenModal(false);
@@ -69,15 +74,25 @@ function PopolForm(){
             return;
         }
 
-        fetch('https://stock-a95d6-default-rtdb.firebaseio.com/.json',{        // 선택 잘 했을때, 서버로 어떤저략인지 데이터 POST. 
-            method:'POST',
-            body:JSON.stringify({sttData}),
-            headers:{
-                'Content-type': 'application/json'
-            }
-        }).then(navigate('/home/popol/select'))             // 그후 장바구니 페이지로 이동
+        
+        navigate('/home/popol/select', {state:{
+            strategy: {sttData}
+        }})             // 장바구니 페이지로 이동 (선택전략정보도 같이 이동)
         
     }
+
+
+
+    // 전략종류 받아오는 API
+
+    useEffect(()=>{
+        fetch('http://haniumproject.com/getModelInfo')
+        .then(response=> response.json())
+        .then(data => {
+            console.log(data);
+            setsttList(data);
+        })
+    },[])
 
     
 
@@ -92,19 +107,18 @@ function PopolForm(){
 
                 <h1> 사용하실 전략을 선택하세요 </h1>
 
-                <SelectButton Clicked={Clicked[0]} onClick={selectstt1}>        
-                    <h2>전략1</h2>
-                    <h3>월간 수익률</h3>
-                    <h3>변동 </h3>
-                    <h3>Sharpe-Ratio</h3>
-                </SelectButton>
-                
-                <SelectButton Clicked={Clicked[1]} onClick={selectstt2}>
-                    <h2>전략2</h2>
-                    <h3>월간 수익률</h3>
-                    <h3>변동 </h3>
-                    <h3>Sharpe-Ratio</h3>
-                </SelectButton>
+                {sttList.map( (item, idx) => {
+                    return(
+                        <SelectButton Clicked={Clicked[idx]} onClick={()=>{selectstt(idx, item.name)}}>        
+                            <h2>전략 {idx+1}</h2>
+                            <h3>name: {item.name}</h3>
+                            <h3>er : {item.er} </h3>
+                            <h3>Sharpe: {item.sharpe}</h3>
+                            <h3>fileName: {item.filename}</h3>
+                        </SelectButton>
+                    )
+                })}
+            
 
                 
                 <button className={classes.submitbtn} onClick={submitHandler}>다음 단계로</button>
