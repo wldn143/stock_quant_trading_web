1c6e89985e9a989c10cd5b187ce0390877b2b93e
박진성 <plashdof@gmail.com>
2022-08-06 19:37:04 +0900

    homepage 즐겨찾기 서버통신 추가

--- src/component/HomeElement/Enjoysearch.js
+++ src/component/HomeElement/Enjoysearch.js
@@ -2,29 +2,45 @@ import classes from './Enjoysearch.module.css';
 import {useEffect, useState} from 'react';
 
 
-// TODO 1 : 받아온 즐겨찾기 리스트 처음 3개 화면에 출력
-// TODO 2 : uuid가 잘 반영되어서 서버로 가는지 체크
-// TODO 3 : 서버에서 받아온 즐겨찾기리스트 데이터 parsing 잘 되는지 체크
-
 function Enjoysearch(){
 
 
     const uuid = sessionStorage.getItem('uuid');
+    let [tostr, settostr] = useState([]);
+    let [tostrPrice, settostrPrice] = useState([]);
 
 
     // 즐겨찾기 리스트 받아오기
 
     useEffect(()=>{
-
         fetch(`http://haniumproject.com/getUserAccount/${uuid}`)
         .then( response => response.json())
         .then( data => {
             console.log(data)
-            let temp = data.favlist.split(",");
-            sessionStorage.setItem('FavList', JSON.stringify(temp));
+            settostr(data.favlist.split(","));
+            console.log(tostr);
         });
-
     },[])
+
+
+    // 즐겨찾기 리스트 가격 받아오기
+
+    useEffect(()=>{
+
+        fetch('http://54.215.210.171:8000/getPreview',{
+            method: 'POST',
+            body:JSON.stringify({
+                code: tostr.slice(undefined,3)
+            }),
+            headers:{
+                'Content-Type' : './application.json'
+            }
+        }).then( response => response.json())
+        .then( data => {
+            tostrPrice(data);
+
+        })
+    }, [tostr]);
     
 
 
@@ -41,18 +57,15 @@ function Enjoysearch(){
             </thead>
 
             <tbody className={classes.tbody}>
-                <tr>
-                    <th className={classes.tobject}>삼성전자</th>
-                    <th className={classes.tprice}>35000</th>
-                </tr>
-                <tr>
-                    <th className={classes.tobject}>LG화학</th>
-                    <th className={classes.tprice}>25000</th>
-                </tr>
-                <tr>
-                    <th className={classes.tobject}>SK이노베이션</th>
-                    <th className={classes.tprice}>150000</th>
-                </tr>
+                {tostr.map((item,idx)=>{
+
+                    return(
+                        <tr>
+                            <th className={classes.tobject}>{item}</th>
+                            <th className={classes.tprice}>{tostrPrice[idx]}</th>
+                        </tr>
+                    )
+                })}
             </tbody>
         </table>
         
